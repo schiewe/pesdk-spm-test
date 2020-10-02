04c64a23c59091d582b7d82c522d3672915e53ec
Sascha Schwabbauer <sascha.schwabbauer@img.ly>
2020-10-02 07:40:09 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -5,11 +5,18 @@ let package = Package(
   name: "PhotoEditorSDK",
   platforms: [.iOS(.v9)],
   products: [
-    .library(name: "ImglyKit", targets: ["ImglyKit"]),
-    .library(name: "PhotoEditorSDK", targets: ["PhotoEditorSDK"])
+    .library(
+        name: "PhotoEditorSDK", 
+        targets: ["ImglyKit", "PhotoEditorSDK"]),
   ],
   targets: [
-    .binaryTarget(name: "ImglyKit", url: "https://github.com/imgly/pesdk-ios-build/releases/download/10.21.1/PhotoEditorSDK.zip", checksum: "e1772322f48a8beb6fb4a5778b8486ffa1b0207a24e3672cfaa9b02e5749c6ce"),
-    .binaryTarget(name: "PhotoEditorSDK", url: "https://github.com/imgly/pesdk-ios-build/releases/download/10.21.1/PhotoEditorSDK.zip", checksum: "e1772322f48a8beb6fb4a5778b8486ffa1b0207a24e3672cfaa9b02e5749c6ce")
+    .binaryTarget(
+        name: "ImglyKit", 
+        url: "https://github.com/imgly/pesdk-ios-build/releases/download/10.21.1/PhotoEditorSDK.zip", 
+        checksum: "e1772322f48a8beb6fb4a5778b8486ffa1b0207a24e3672cfaa9b02e5749c6ce"),
+    .binaryTarget(
+        name: "PhotoEditorSDK", 
+        url: "https://github.com/imgly/pesdk-ios-build/releases/download/10.21.1/PhotoEditorSDK.zip", 
+        checksum: "e1772322f48a8beb6fb4a5778b8486ffa1b0207a24e3672cfaa9b02e5749c6ce")
   ]
 )
