fae8b9ae79c425a8c691e7e97da705dc85aef1af
Sascha Schwabbauer <sascha.schwabbauer@img.ly>
2020-10-02 11:05:05 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -13,10 +13,10 @@ let package = Package(
     .binaryTarget(
         name: "ImglyKit", 
         url: "https://www.dropbox.com/s/fyps5h9ntvactja/ImglyKit.zip?dl=1", 
-        checksum: "21e26434f80af880302a14bbffce7368ef9fc1bdba4f94d5ff9f6e458a19718a"),
+        checksum: "4e580cf3109e3c55e34259baa84d06257eee0906b16f48376e32afa80aeb238a"),
     .binaryTarget(
         name: "PhotoEditorSDK", 
         url: "https://www.dropbox.com/s/55orhpg3tkenszf/PhotoEditorSDK.zip?dl=1", 
-        checksum: "45eee992289a64e7f05edf1d2d1ceba977418eb524ce1885f31b97e2e57770a1")
+        checksum: "d90561e2699e20c3fc4e3ec7ac88f3a4a179ae9147ec422033049f2c3571928c")
   ]
 )
