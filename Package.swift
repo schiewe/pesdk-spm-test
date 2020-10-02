29b18de3239d757c66bf1a76ff62c28f0db0c45b
Sascha Schwabbauer <sascha.schwabbauer@img.ly>
2020-10-02 12:28:46 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -3,19 +3,18 @@ import PackageDescription
 
 let package = Package(
   name: "PhotoEditorSDK",
+  dependencies: [
+    .package(url: "https://github.com/sascha/imglykit-spm-test.git", .branch("main"))
+  ],
   platforms: [.iOS(.v9)],
   products: [
-    .library(name: "ImglyKit", targets: ["ImglyKit"]),
     .library(name: "PhotoEditorSDK", targets: ["PhotoEditorSDK"]),
   ],
   targets: [
     .binaryTarget(
-        name: "ImglyKit", 
-        url: "https://www.dropbox.com/s/fyps5h9ntvactja/ImglyKit.zip?dl=1", 
-        checksum: "4e580cf3109e3c55e34259baa84d06257eee0906b16f48376e32afa80aeb238a"),
-    .binaryTarget(
         name: "PhotoEditorSDK", 
         url: "https://www.dropbox.com/s/55orhpg3tkenszf/PhotoEditorSDK.zip?dl=1", 
-        checksum: "d90561e2699e20c3fc4e3ec7ac88f3a4a179ae9147ec422033049f2c3571928c")
+        checksum: "d90561e2699e20c3fc4e3ec7ac88f3a4a179ae9147ec422033049f2c3571928c",
+        dependencies: ["ImglyKit"])
   ]
 )
